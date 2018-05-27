ca1e2358bba0a04e16b95bf2f9b1921bb6b456d3
wonjaechoi <wvvwvwwv@naver.com>
2018-05-27 19:27:44 +0900

    temp

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Arch;
+
+public class PlayerControl : MonoBehaviour {
+    public static PlayerControl instance;
+	void Awake()
+    {
+        instance = this;
+        player = GetComponent<Player>();
+    }
+    void Start () {
+        Input.multiTouchEnabled = false;
+	}
+
+
+	public void InitPlayer(Room room)
+	{
+		CameraFollow.instance.RoomTrace(room);
+		player.SetRoom(room, new Vector2Int(4, 4));
+		GameManager.instance.SetCurrentRoom (room);
+		GameManager.instance.OnPlayerEnterNewRoom ();	
+	}
+
+    private Player player;
+
+	private List<Tile> path;
+	public int GetRemainAction(){
+		if (path == null) {
+			return 0;
+		}
+		return path.Count;
+	}
+
+	public bool ActionResume(){
+		if (player.MoveTo (path [0].pos)) {
+			path.RemoveAt (0);
+			return true;
+		} else {
+			path = null;
+			return false;
+		}
+	}
+
+	public bool PlayerMoveCommand(Tile pos)
+    {
+		Room cur = player.currentRoom;
+		int curHp = player.currentHp;
+		path = PathFinding.instance.GeneratePath(player, pos);
+
+		return ActionResume ();
+    }
+
+	#region Card
+	public Deck deck;
+	public HandCard hand;
+
+	public void DrawCard(){
+		hand.DrawHand (deck.Draw ());
+	}
+
+	public void ReLoadDeck(){
+		deck.Load ();
+	}
+
+	/// <summary>
+	/// Call from GameManager
+	/// </summary>
+	public void OnStartPlayerTurn(){
+		hand.CheckAvailable ();
+	}
+
+	/// <summary>
+	/// Call from GameManager(Move) or CardObject(Card)
+	/// </summary>
+	public void EndTurn(){
+		//TODO : GameManager End Callback
+	}
+
+	public void BattleEncount(){
+
+	}
+
+
+	public void EndBattle(){
+
+
+
+	}
+
+	private void ShowHand(){
+
+	}
+
+	private void HideHand(){
+
+	}
+	#endregion
+}
